a9f12cb1d3027f8d3c36daf83a4f2d32eac52c79
SELMA B <selmabasheer30@gmail.com>
2026-06-25 21:10:11 +0530

    feat: add order service api layer with swagger and exception handling

--- server/OrderService/order-service/src/main/java/com/nutrihub/orderservice/OrderServiceApplication.java
+++ server/OrderService/order-service/src/main/java/com/nutrihub/orderservice/OrderServiceApplication.java
@@ -3,11 +3,20 @@ package com.nutrihub.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+
+		try {
+			Desktop.getDesktop().browse(new URI("http://localhost:5008/swagger-ui.html"));
+		} catch (Exception e) {
+			System.out.println("Open Swagger at: http://localhost:5008/swagger-ui.html");
+		}
 	}
 
 }
