b6a728c35fe3a1946708703e16e4813d8ddd45b6
SELMA B <selmabasheer30@gmail.com>
2026-06-26 17:36:55 +0530

    feat: add api layer

--- server/InventoryService/inventory-service/src/main/java/com/nutrihub/inventoryservice/InventoryServiceApplication.java
+++ server/InventoryService/inventory-service/src/main/java/com/nutrihub/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,23 @@
-package com.nutrihub.inventoryservice;
+	package com.nutrihub.inventoryservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class InventoryServiceApplication {
+	import java.awt.*;
+	import java.net.URI;
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+	@SpringBootApplication
+	public class InventoryServiceApplication {
+
+		public static void main(String[] args) {
+
+			SpringApplication.run(InventoryServiceApplication.class, args);
+
+			try {
+				Desktop.getDesktop().browse(new URI("http://localhost:5009/swagger-ui.html"));
+			} catch (Exception e) {
+				System.out.println("Open Swagger at: http://localhost:5009/swagger-ui.html");
+			}
+		}
+
 	}
-
-}
